953834f1f40b55fe0eac7a69d6ca0bb4a252471a
wld <d1zciplineo@gmail.com>
2024-06-04 19:16:10 +0300

    Misc commit

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -1,4 +1,30 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-vim.keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+local keymap = vim.keymap
+local opts = { noremap = true, silent = true }
+
+keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+
+-- Select all
+keymap.set("n", "<C-a>", "gg<S-v>G")
+
+-- Save file and quit
+keymap.set("n", "<Leader>w", ":update<Return>", opts)
+keymap.set("n", "<Leader>q", ":quit<Return>", opts)
+keymap.set("n", "<Leader>Q", ":qa<Return>", opts)
+
+-- File explorer with NvimTree
+keymap.set("n", "<Leader>f", ":NvimTreeFindFile<Return>", opts)
+keymap.set("n", "<Leader>t", ":NvimTreeToggle<Return>", opts)
+
+-- Tabs
+keymap.set("n", "te", ":tabedit")
+keymap.set("n", "<tab>", ":tabnext<Return>", opts)
+keymap.set("n", "<s-tab>", ":tabprev<Return>", opts)
+keymap.set("n", "tw", ":tabclose<Return>", opts)
+
+-- diagnostic
+keymap.set("n", "<C-j>", function()
+  vim.diagnostic.goto_next()
+end, opts)
