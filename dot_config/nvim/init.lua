6bb072e64d34c2766e7fc6bcc07d1d929c0d4869
wld <d1zciplineo@gmail.com>
2024-06-06 08:04:38 +0300

    Setting up vscode neovim

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,16 +1,17 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 if vim.g.vscode then
   -- VSCode extension
-  -- <leader> key
-  vim.g.mapleader = " "
-  -- clear search highlightning
-  vim.keymap.set("n", "<Esc>", ":nohlsearch<cr>")
-  -- search ignoring case
-  vim.opt.ignorecase = true
-  vim.opt.smartcase = true
-  -- sync system clipboard
-  vim.opt.clipboard = "unnamedplus"
-  -- vim.opt.scrolloff = 4
+  require("config.lazy")
+  -- -- <leader> key
+  -- vim.g.mapleader = " "
+  -- -- clear search highlightning
+  -- vim.keymap.set("n", "<Esc>", ":nohlsearch<cr>")
+  -- -- search ignoring case
+  -- vim.opt.ignorecase = true
+  -- vim.opt.smartcase = true
+  -- -- sync system clipboard
+  -- vim.opt.clipboard = vim.env.SSH_TTY and "" or "unnamedplus"
+  -- -- vim.opt.scrolloff = 4
 else
   -- ordinary Neovim
   require("config.lazy")
--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -5,6 +5,7 @@ local keymap = vim.keymap
 local opts = { noremap = true, silent = true }
 
 keymap.set("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
+keymap.set("n", "<Esc>", ":nohlsearch<Return>")
 
 -- Select all
 keymap.set("n", "<C-a>", "gg<S-v>G")
